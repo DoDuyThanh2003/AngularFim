9ac9ff718ec6250ac6e60b152874a3d122b918aa
DoDuyThanh2303 <doduythanh2303@gmail.com>
2025-04-24 11:56:37 +0700

    addddd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboadLayoutComponent } from './pages/dashboad-layout/dashboad-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:'dashboad',
+        pathMatch:'full'
+    },
+    {
+        path:'dashboad',
+        component:DashboadLayoutComponent
+    }
+];
---
+++ src/app/pages/dashboad-layout/dashboad-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderLayoutComponent } from '../header-layout/header-layout.component';
+
+@Component({
+  selector: 'app-dashboad-layout',
+  imports: [HeaderLayoutComponent],
+  templateUrl: './dashboad-layout.component.html',
+  styleUrl: './dashboad-layout.component.scss'
+})
+export class DashboadLayoutComponent {
+}
